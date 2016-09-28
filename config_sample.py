d292406de804c46bddc69d1ed2213ddc8d8c198e
pradyumna chatterjee <pradyumnacster.com>
2016-09-28 06:28:32 +0530

    Electoral sentiment analysis for Modi, Trump, Obama, Clinton

--- config_sample.py
+++ config_sample.py
@@ -1,4 +1,4 @@
-consumer_key = "Your_consumer_key_here"
-consumer_secret = "Your_consumer_secret_here"
-access_token = "Your_access_token_here"
-access_token_secret = "Your_access_token_secret_here"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
--- sentiment.py
+++ sentiment.py
@@ -8,45 +8,59 @@ from elasticsearch import Elasticsearch
 # import twitter keys and tokens
 from config import *
 
+ELASTIC_SEARCH_HOST = '192.168.99.100'
+ELASTIC_SEARCH_PORT = 9900
+
+
 # create instance of elasticsearch
-es = Elasticsearch()
+es = Elasticsearch(
+    hosts=[{'host': ELASTIC_SEARCH_HOST , 'port': ELASTIC_SEARCH_PORT}],
+)
 
 
 class TweetStreamListener(StreamListener):
 
     # on success
     def on_data(self, data):
+        try:
+            # decode json
+            dict_data = json.loads(data)
 
-        # decode json
-        dict_data = json.loads(data)
+            # pass tweet into TextBlob
+        
+            tweet = TextBlob(dict_data["text"])
+        
+            # output sentiment polarity
+            print tweet.sentiment.polarity
 
-        # pass tweet into TextBlob
-        tweet = TextBlob(dict_data["text"])
+            # determine if sentiment is positive, negative, or neutral
+            if tweet.sentiment.polarity < 0:
+                sentiment = "negative"
+            elif tweet.sentiment.polarity == 0:
+                sentiment = "neutral"
+            else:
+                sentiment = "positive"
 
-        # output sentiment polarity
-        print tweet.sentiment.polarity
+            # output sentiment
+            print sentiment
 
-        # determine if sentiment is positive, negative, or neutral
-        if tweet.sentiment.polarity < 0:
-            sentiment = "negative"
-        elif tweet.sentiment.polarity == 0:
-            sentiment = "neutral"
-        else:
-            sentiment = "positive"
-
-        # output sentiment
-        print sentiment
-
-        # add text and sentiment info to elasticsearch
-        es.index(index="sentiment",
-                 doc_type="test-type",
-                 body={"author": dict_data["user"]["screen_name"],
-                       "date": dict_data["created_at"],
-                       "message": dict_data["text"],
-                       "polarity": tweet.sentiment.polarity,
-                       "subjectivity": tweet.sentiment.subjectivity,
-                       "sentiment": sentiment})
-        return True
+            # add text and sentiment info to elasticsearch
+            es.index(index="sentiment",
+                     doc_type="test-type",
+                     body={"author": dict_data["user"]["screen_name"],
+                           "date": dict_data["created_at"],
+                           "message": dict_data["text"],
+                           "polarity": tweet.sentiment.polarity,
+                           "subjectivity": tweet.sentiment.subjectivity,
+                           "sentiment": sentiment})
+            return True
+        except: 
+            print "Error Detected:"
+            return True
+            # print dict_data["text"].encode('utf-8')
+            
+        
+        
 
     # on failure
     def on_error(self, status):
@@ -65,4 +79,4 @@ if __name__ == '__main__':
     stream = Stream(auth, listener)
 
     # search twitter for "congress" keyword
-    stream.filter(track=['congress'])
+    stream.filter(track=['Modi','Obama','Trump','Clinton'])
